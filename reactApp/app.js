87a5848a26783a73e4102a2c900402b8024fc15e
bvi1994 <bvi1@ucsc.edu>
2017-10-06 14:50:36 -0700

    Added dummy finished-items that works and a textbox with button

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,15 +3,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var dummyData = ['Pika', "Pikachu", "Chu", "Chaaaaaa...", "Pika Pika Pikachu Pi Pi"]
+//var dummyData = ['Pika', "Pikachu", "Chu", "Chaaaaaa...", "Pika Pika Pikachu Pi Pi"]
+var dummyData = [
+  {taskText: 'Pika', completed: true},
+  {taskText: 'Pikachu', completed: false},
+  {taskText: 'Chu', completed: false},
+  {taskText: 'Chaaaaaa...', completed: true},
+  {taskText: 'Pika Pika Pikachu Pi Pi', completed: false}
+]
 
 class TodoList extends React.Component{
   render(){
     return(
-      <ul>{dummyData.map(Todo => <li>
-           <button type="button">X</button> {Todo}
-      </li>)
-    }</ul>
+      <div>
+        <input type='text' placeholder='Enter Todo Item here'></input> <button type="button">Add To-Do Item</button>
+        <ul>{dummyData.map(Todo => <li>
+             <button type="button">x</button> {Todo.completed ? Todo.taskText : <strike>{Todo.taskText}</strike>}
+        </li>)
+      }</ul>
+    </div>
     )
   }
 }
